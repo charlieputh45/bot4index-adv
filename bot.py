d8a7bc8059f886df45f56e0e2326741e24974d54
Charlie Pathak <130906410+charlieputh45@users.noreply.github.com>
2025-06-13 12:12:20 +0000

    added safe api for pm filter

--- bot.py
+++ bot.py
@@ -135,7 +135,7 @@ async def channel_file_handler(client, message):
     media = message.video or message.document or message.audio
     caption = await remove_unwanted(message.caption if message.caption else media.file_name)
     async with copy_lock:
-        cpy_msg = await message.copy(TMDB_CHANNEL_ID, caption=f"<code>{caption}</code>", parse_mode=enums.ParseMode.HTML)
+        cpy_msg = await safe_api_call(message.copy(TMDB_CHANNEL_ID, caption=f"<code>{caption}</code>", parse_mode=enums.ParseMode.HTML))
     await file_handler(cpy_msg)
     await message.delete()
 
